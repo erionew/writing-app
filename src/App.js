a33bbc7fd072f11978b881290496e2d4e2f4f443
Erione White <erionewhite1999@gmail.com>
2023-05-06 19:17:52 -0500

    initial commit, created component for project page and started styling

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import AllProjects from './components/AllProjects';
+
+function App() {
+  return (
+    <div className="App">
+      <AllProjects />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/AllProjects.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function AllProjects() {
+  return (
+    <div container--page>
+        <header>
+          <h1 className='h1--page-title'>Projects</h1>
+          <button className='button--blue'>New Project +</button>
+        </header>
+        <div className='container--projects'>
+
+        </div>
+    </div>
+  )
+}
